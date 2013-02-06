b2813219abb5ca686ac01e848e4bcf801be372a9
James McKinney <james@slashpoundbang.com>
2013-02-06 03:34:32 -0500

    remove unneeded initializers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-OpenGovernment::Application.config.session_store :cookie_store, key: '_opengovernment-app_session'
+OpenGovernment::Application.config.session_store :cookie_store, key: '_opengovernment_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
