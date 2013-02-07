01507b925a6daef674e62d5f8f16b56acc187cd6
James McKinney <james@slashpoundbang.com>
2013-02-07 13:55:07 -0500

    run popolo generator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  rescue_from Mongoid::Errors::DocumentNotFound, Popolo::ImproperlyNestedResource do |exception|
+    respond_to do |format|
+      format.html { render file: Rails.root.join('public', '404.html'), status: :not_found }
+      format.json { head :not_found }
+      format.atom { head :not_found }
+    end
+  end
   protect_from_forgery
 end
