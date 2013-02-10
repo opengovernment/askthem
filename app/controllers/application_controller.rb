ff00dc44ee1dd5b493ab8a4715447754de0c80d7
James McKinney <james@slashpoundbang.com>
2013-02-10 15:35:09 -0500

    basic views for org hierarchy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  rescue_from Mongoid::Errors::DocumentNotFound, Popolo::ImproperlyNestedResource do |exception|
+  rescue_from Mongoid::Errors::DocumentNotFound do |exception|
     respond_to do |format|
       format.html { render file: Rails.root.join('public', '404.html'), status: :not_found }
       format.json { head :not_found }
