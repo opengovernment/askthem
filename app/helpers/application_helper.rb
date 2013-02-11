f2f9e57d0ea85bdb34abf6b649521943acc100ef
James McKinney <james@slashpoundbang.com>
2013-02-11 09:53:54 -0500

    move basic views to popolo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  # @note inherited_resources sets incorrect arguments to glob routes.
-  # @see https://github.com/josevalim/inherited_resources/blob/master/lib/inherited_resources/url_helpers.rb
-  def nested_resources_path(*args)
-    send("nested_#{resource_collection_name}_path", *args)
-  end
-  def nested_resource_path(*args)
-    send("nested_#{resource_instance_name}_path", *args)
-  end
 end
