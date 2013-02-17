e1cdaaf18481d5224bbf5a1b73c3b8800fd2f298
James McKinney <james@slashpoundbang.com>
2013-02-17 12:17:24 -0500

    tidy post view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,43 @@
 module ApplicationHelper
   # @param [String] url an image URL
-  # @param [Integer] width the maximum image width
-  # @param [Integer] height the maximum image height
   # @param [Hash] opts optional arguments
-  def cloudfront(url, width, height, opts = {})
-    image_tag("http://d2xfsikitl0nz3.cloudfront.net/#{CGI.escape(url)}/#{width}/#{height}", opts)
+  # @option opts [Integer] :width the maximum image width
+  # @option opts [Integer] :height the maximum image height
+  def cdn_image_tag(url, opts = {})
+    width, height = opts[:size].split('x')
+    image_tag("http://#{ENV['CLOUDFRONT_DOMAIN_NAME']}/#{CGI.escape(url)}/#{width}/#{height}", opts)
+  end
+
+  def jurisdiction_name
+    if @area
+      @area.root.name
+    elsif @organization && @organization.area
+      @organization.area.root.name
+    elsif @post && @post.area
+      @post.area.root.name
+    end
+  end
+
+  def subheader_on_posts(post)
+    parts = []
+    parts << post.person['party'] if post.person && post.person['party']
+    parts << area_name(post.area) if post.area
+    parts.join(', ')
+  end
+
+  def subheader_on_post(post)
+    parts = []
+    parts << post.person['party'] if post.person && post.person['party']
+    parts << post.role
+    parts << area_name(post.area) if post.area
+    parts.join(', ')
+  end
+
+  def area_name(area)
+    if area.name[/\A[\dA-Z]+\z/]
+      "District #{area.name}"
+    else
+      area.name
+    end
   end
 end
