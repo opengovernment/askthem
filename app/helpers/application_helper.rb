ce5f03ec92737b891c96094695eb47fe1e5a3ccd
James McKinney <james@slashpoundbang.com>
2013-02-17 12:17:10 -0500

    basic post view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def cloudfront(url, width, height)
-    image_tag("http://d2xfsikitl0nz3.cloudfront.net/#{CGI.escape(url)}/#{width}/#{height}", alt: '')
+  # @param [String] url an image URL
+  # @param [Integer] width the maximum image width
+  # @param [Integer] height the maximum image height
+  # @param [Hash] opts optional arguments
+  def cloudfront(url, width, height, opts = {})
+    image_tag("http://d2xfsikitl0nz3.cloudfront.net/#{CGI.escape(url)}/#{width}/#{height}", opts)
   end
 end
