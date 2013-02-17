eadd887e45d6727a21a1215efa20ee0f7862e17b
James McKinney <james@slashpoundbang.com>
2013-02-17 00:21:04 -0500

    remove people views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def cloudfront(url, width, height)
-    image_tag("http://d2xfsikitl0nz3.cloudfront.net/#{CGI.escape(url)}/#{width}/#{height}")
+    image_tag("http://d2xfsikitl0nz3.cloudfront.net/#{CGI.escape(url)}/#{width}/#{height}", alt: '')
   end
 end
