8e9cef3d2f94ef297026ed3b0748320bd4f29641
James McKinney <james@slashpoundbang.com>
2013-02-20 19:26:57 -0500

    enhance sanity.js

--- script/sanity.js
+++ script/sanity.js
@@ -59,8 +59,10 @@ db.metadata.find().forEach(function (obj) {
   for (chamber in obj.chambers) {
     chambers.push(chamber);
   }
-  var districts = db.districts.distinct('name', {abbr: obj._id});
 
+  // @note Can add `enum` to districts.json and person.json schemas.
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/bill.json#L112
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/vote.json#L5
   ['bills', 'districts', 'legislators', 'votes'].forEach(function (collection) {
     reportList(collection, 'chamber', {
       state: obj._id,
@@ -71,8 +73,36 @@ db.metadata.find().forEach(function (obj) {
     }, obj._id.toUpperCase() + ' ' + collection + ' with invalid chamber');
   });
 
-  var chambers_plus_other = chambers.concat(['joint', 'other']);
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/bill.json#L152
+  reportList('bills', 'sponsors.chamber', {
+    state: obj._id,
+    'sponsors.chamber': {
+      '$exists': true,
+      '$nin': chambers,
+    },
+  }, obj._id.toUpperCase() + ' bills with invalid sponsors.chamber');
 
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/vote.json#L6
+  reportList('votes', 'bill_chamber', {
+    state: obj._id,
+    bill_chamber: {
+      '$exists': true,
+      '$nin': chambers,
+    },
+  }, obj._id.toUpperCase() + ' votes with invalid bill_chamber');
+
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/bill.json#L103
+  var chambers_plus_null = chambers.concat([null]);
+  reportList('bills', 'companions.chamber', {
+    state: obj._id,
+    'companions.chamber': {
+      '$exists': true,
+      '$nin': chambers_plus_null,
+    },
+  }, obj._id.toUpperCase() + ' bills with invalid companions.chamber');
+
+  // @note Can add `enum` property to events.json schema.
+  var chambers_plus_other = chambers.concat(['joint', 'other']);
   reportList('events', 'participants.chamber', {
     state: obj._id,
     'participants.chamber': {
@@ -81,8 +111,8 @@ db.metadata.find().forEach(function (obj) {
     },
   }, obj._id.toUpperCase() + ' events with invalid participants.chamber (e.g. "Senate")');
 
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/committee.json#L6
   var chambers_plus_joint = chambers.concat(['joint']);
-
   reportList('committees', 'chamber', {
     state: obj._id,
     'chamber': {
@@ -91,6 +121,7 @@ db.metadata.find().forEach(function (obj) {
     },
   }, obj._id.toUpperCase() + ' committees with invalid chamber');
 
+  // @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/person.json#L11
   reportList('legislators', 'roles.chamber', {
     state: obj._id,
     'roles.chamber': {
@@ -99,12 +130,13 @@ db.metadata.find().forEach(function (obj) {
     },
   }, obj._id.toUpperCase() + ' legislators with invalid roles.chamber');
 
+  var districts = db.districts.distinct('name', {abbr: obj._id});
   ['district', 'roles.district'].forEach(function (field) {
-    var criteria = {state: obj._id}
+    var criteria = {state: obj._id};
     criteria[field] = {
       '$exists': true,
       '$nin': districts,
-    }
+    };
     reportList('legislators', field, criteria, obj._id.toUpperCase() + ' legislators with invalid ' + field);
   });
 });
@@ -125,11 +157,13 @@ reportList('legislators', 'party', {
 });
 
 // Are any addresses nearly blank?
+// @note Can add `minLength` property to person.json schema.
 reportList('legislators', 'offices.address', {
   'offices.address': ',',
 }, 'Legislators with invalid offices.address (",")');
 
 // Are all genders taken from a code list?
+// @note Can add `enum` property if `gender` is added to the schema.
 reportList('legislators', '+gender', {
   '+gender': {
     '$exists': true,
@@ -138,6 +172,7 @@ reportList('legislators', '+gender', {
 });
 
 // Are all office types taken from a code list?
+// @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/person.json#L27
 reportList('legislators', 'offices.type', {
   'offices': {
     '$ne': [],
@@ -147,13 +182,39 @@ reportList('legislators', 'offices.type', {
   },
 });
 
+// Are all bill actors taken from a code list?
+reportList('bills', 'actions.actor', {
+  'actions.actor': {
+    '$exists': true,
+    '$nin': [/^lower/, /^upper/, 'other', 'executive'],
+  },
+});
+
+// Are all bill sponsor types taken from a code list?
+// @see https://github.com/sunlightlabs/billy/blob/master/billy/schemas/bill.json#L152
+reportList('bills', 'sponsors.type', {
+  'sponsors.type': {
+    '$exists': true,
+    '$nin': ['primary', 'cosponsor'],
+  },
+});
+
 // Are any photo URLs relative paths?
+// @note Can add `pattern` property to person.json schema.
 reportList('legislators', 'photo_url', {
   photo_url: {
     '$exists': true,
     '$nin': ['', null, /^http/],
   },
 });
+
+// Are any photo URLs blank?
+reportTotal('legislators', {
+  photo_url: {
+    '$exists': true,
+    '$in': ['', null],
+  },
+}, 'have a blank photo_url');
 
 // Any spaces in IDs?
 ['transparencydata_id', 'nimsp_id', 'nimsp_candidate_id', 'votesmart_id'].forEach(function (field) {
@@ -171,14 +232,6 @@ reportList('legislators', 'photo_url', {
   };
   reportTotal('legislators', criteria2, 'have a blank ' + field + ' ("" or null)');
 });
-
-// Are any photo URLs blank?
-reportTotal('legislators', {
-  photo_url: {
-    '$exists': true,
-    '$in': ['', null],
-  },
-}, 'have a blank photo_url');
 
 // Used as part of a poor man's sprintf to align party counts.
 var pad = '                                        ';
