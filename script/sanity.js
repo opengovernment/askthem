fb0c9ed9dbd399fd02a61b375104986ef75846ce
James McKinney <james@slashpoundbang.com>
2013-02-21 12:25:13 -0500

    report unique members.role

--- script/sanity.js
+++ script/sanity.js
@@ -345,17 +345,27 @@ reportTotal('legislators', {
   reportTotal('legislators', criteria2, 'have a blank ' + field + ' ("" or null)');
 });
 
-// Manual //////////////////////////////////////////////////////////////////////
+// Manually review /////////////////////////////////////////////////////////////
 
 // Used as part of a poor man's sprintf to align party counts.
 var pad = '                                        ';
 
-// Manually review the list of party names.
 print('\nDistinct parties for manual review:');
 db.legislators.mapReduce(function () {
   if (this.party) {
     emit(this.party, 1);
   }
+}, function (key, values) {
+  return Array.sum(values);
+}, {out: {inline: 1}}).results.forEach(function (result) {
+  print(result._id + pad.substring(0, 40 - result._id.length) + result.value);
+});
+
+print('\nDistinct members.roles for manual review:');
+db.committees.mapReduce(function () {
+  this.members.forEach(function (member) {
+    emit(member.role, 1);
+  });
 }, function (key, values) {
   return Array.sum(values);
 }, {out: {inline: 1}}).results.forEach(function (result) {
