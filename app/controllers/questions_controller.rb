922793a23f90e35e71ab236be6c510ce8d7596b0
James McKinney <james@slashpoundbang.com>
2013-02-27 19:40:30 -0500

    stub questions so they can be seen

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,16 @@ class QuestionsController < ApplicationController
   inherit_resources
   respond_to :html
   actions :index, :show, :new
+
+  before_filter :set_jurisdiction
+
+  def show
+    # @todo stub
+  end
+
+private
+
+  def collection
+    @questions ||= end_of_association_chain # @todo limit to jurisdiction
+  end
 end
