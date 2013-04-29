27ecb9ffc943c75e8ece088f9c1e9952b00c823e
James McKinney <james@slashpoundbang.com>
2013-04-29 15:32:15 -0400

    add default_url_options in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ OpenGovernment::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {host: 'oglocal.herokuapp.com'}
 
   # Enable threaded mode
   # config.threadsafe!
