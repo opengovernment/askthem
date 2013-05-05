d47d1be61888c2f1f001425cb0dba97c42a25ec2
James McKinney <james@slashpoundbang.com>
2013-05-05 14:56:41 -0400

    avoid error

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -40,18 +40,25 @@ private
   end
 
   def stub # @todo remove once we add some questions
-    bill = Bill.in(parent.abbreviation).last
-    Question.new({
+    attributes = {
       title: 'What for dost doth under thy venerable name?',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu arcu augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat, enim ac suscipit convallis, sem ligula molestie sem, nec adipiscing nulla leo id lacus. Donec in orci sed erat blandit ornare. Nunc non quam sit amet quam suscipit tristique sed a velit. Nunc cursus pellentesque lectus a tincidunt. Nam blandit, mi ac suscipit lobortis, mauris purus vulputate dui, ac volutpat sapien nibh nec nisl. Duis faucibus rutrum placerat. Pellentesque semper laoreet nisl eget hendrerit. Aliquam erat volutpat. Morbi ligula augue, vehicula ut fermentum in, porttitor sit amet sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eu varius tellus. Maecenas neque sem, egestas id venenatis nec, aliquam congue felis.',
       state: parent.abbreviation,
       subject: 'Health',
       user: User.first,
       person: Person.in(parent.abbreviation).last,
-      bill: bill,
-      bill_id: bill.try(:id),
       issued_at: 1.week.ago,
-    })
+    }
+
+    bill = Bill.in(parent.abbreviation).last
+    if bill
+      attributes.merge!({
+        bill: bill,
+        bill_id: bill.id,
+      })
+    end
+
+    Question.new(attributes)
   end
   
 end
