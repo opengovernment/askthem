97742f439dfe459a82ff14cf539e0efa1e423983
Walter McGinnis <wm@waltermcginnis.com>
2013-05-17 21:08:37 +1200

    Temporarily stop VCR based failure from happening
    As travisci builds don't include the same api key as local development does
    vcr was not matching the url of request. Need to find workaround.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -9,9 +9,10 @@ describe Person do
     @person.save!
   end
 
-  describe '.for_location', :vcr do
-    it 'returns matching people given a location' do
+  describe '.for_location' do
+    it 'returns matching people given a location', :vcr do
       address = '2227 Paine Turnpike South, Berlin, VT'
+      pending 'a workaround for vcr matching url of request that includes missing api key'
       expect(Person.with(session: 'openstates').for_location(address).first).to eq @person
     end
   end
