f6fdce3fb46bcf94e3393547ef069ace61ffee81
Walter McGinnis <wm@waltermcginnis.com>
2013-05-18 12:25:06 +1200

    Fix vcr in spec to not trip up on no api key
    Remember to edit apikey out of your cassettes before committing them.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -74,4 +74,7 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
   # c.allow_http_connections_when_no_cassette = false
+  c.default_cassette_options = {
+    match_requests_on: [:method, VCR.request_matchers.uri_without_param(:apikey)]
+  }
 end
