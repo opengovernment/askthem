1928d4bd98f488ecfe360c6f6b58069de2dddffe
James McKinney <james@slashpoundbang.com>
2013-05-19 16:30:52 -0400

    populate subjects dropdown

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,4 +3,8 @@ module QuestionsHelper
   def step_class_for(step, relevant_steps)
     raw('class="not-first-step"') if relevant_steps.first.to_s != step
   end
+
+  def subjects
+    Bill.with(session: 'openstates').distinct('subjects').sort
+  end
 end
