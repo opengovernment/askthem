24b2d440a4b19a6218ff0e8eccb4adbf6b651ca0
James McKinney <james@slashpoundbang.com>
2013-05-19 18:26:02 -0400

    hardcode list of subjects - they never change

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -4,7 +4,52 @@ module QuestionsHelper
     raw('class="not-first-step"') if relevant_steps.first.to_s != step
   end
 
+  # @return [Array<String>] the list of Project Vote Smart subjects
   def subjects
-    Bill.with(session: 'openstates').distinct('subjects').sort
+    [
+      'Abortion',
+      'Abortion and Reproductive',
+      'Agriculture and Food',
+      'Animals and Wildlife',
+      'Arts and Entertainment',
+      'Budget, Spending and Taxes',
+      'Business and Consumers',
+      'Campaign Finance and Elections',
+      'Civil Liberties and Civil Rights',
+      'Conservative',
+      'Crime',
+      'Defense',
+      'Drugs',
+      'Education',
+      'Employment and Affirmative Action',
+      'Energy',
+      'Environment',
+      'Executive Branch',
+      'Federal, State and Local Relations',
+      'Foreign Affairs',
+      'Gambling and Gaming',
+      'Government Operations',
+      'Guns',
+      'Health and Health Care',
+      'Housing and Property',
+      'Immigration',
+      'Judicial Branch',
+      'Labor Unions',
+      'Legislative Branch',
+      'Liberal',
+      'Marriage, Family, and Children',
+      'Military Personnel',
+      'National Security',
+      'Science',
+      'Senior Citizens',
+      'Sexual Orientation and Gender Identity',
+      'Social',
+      'Technology and Communication',
+      'Trade',
+      'Transportation',
+      'Unemployed and Low-Income',
+      'Veterans',
+      'Women',
+    ]
   end
 end
