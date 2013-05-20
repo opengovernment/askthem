34377ebce3a87844a5d5a84817882aa718175574
Walter McGinnis <wm@waltermcginnis.com>
2013-05-20 12:47:15 +1200

    Adding mailtrap for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,4 +63,14 @@ OpenGovernment::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  if ENV['MAILTRAP_HOST'].present?
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['MAILTRAP_USER_NAME'],
+      :password => ENV['MAILTRAP_PASSWORD'],
+      :address => ENV['MAILTRAP_HOST'],
+      :port => ENV['MAILTRAP_PORT'],
+      :authentication => :plain
+    }
+  end
 end
