dad6892aa1b1be475f7fd7b62e2334ca4e73cf93
Walter McGinnis <wm@waltermcginnis.com>
2013-06-27 14:58:58 +1200

    Enable saving and display of  question subject
    Centralizing possible subjects under a class so they can be referred
    to uniformly in validation as well as in UI.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,14 +31,9 @@ class QuestionsController < ApplicationController
     new!
   end
 
-  # TODO: add validation to view and UX
   def create
     @question = Question.new(params[:question])
     @question.state = @state_code
-    # TEMP: our handed code subjects are not consistent as foreign keys
-    # against bills for jurisdiction
-    # TODO: remove this when subject is sorted out
-    @question.subject = nil
     @person = @question.person if @question.person_id.present?
     @user = @question.user
 
