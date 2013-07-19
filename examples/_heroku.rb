635b7fade757ceb0a563ac462915e2ffdcb167f3
Walter McGinnis <wm@waltermcginnis.com>
2013-07-19 14:36:04 +1200

    Add & use AWS_HOST_STUB
    Useful for federal legislator photo lookup.

--- examples/_heroku.rb
+++ examples/_heroku.rb
@@ -1,4 +1,8 @@
+# required environmental variables
+#
 # Sign up at http://services.sunlightlabs.com/accounts/register/
 # ENV['SUNLIGHT_API_KEY'] ||= ''
 # ENV['FACEBOOK_APP_SECRET'] ||= ''
 # ENV['PROJECT_VOTE_SMART_API_KEY'] ||= ''
+# ENV['AWS_DIRECTORY'] ||= ''
+# ENV['AWS_HOST_STUB'] ||= '.s3.amazonaws.com'
