63f2f8f192094c0e25e6b8ec8008de4ea401c143
Derek Eder <derek.eder@gmail.com>
2013-08-01 21:18:35 -0500

    removed unnecessary () from Meeting spec test

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -8,7 +8,7 @@ end
 
 describe '.load_from_api_for_jurisdiction' do
   it 'loads meetings into database given a municipality', :vcr do
-    Meeting.load_from_apis_for_jurisdiction()
+    Meeting.load_from_apis_for_jurisdiction
     expect(Meeting.distinct(:name).length).to be >= 27 # distinct meeting names for Philly
   end
 end
