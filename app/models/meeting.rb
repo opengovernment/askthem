60ba974579c05304bd95be6d1b71a9bc66fec094
Walter McGinnis <wm@waltermcginnis.com>
2013-08-02 14:48:14 +1200

    Merging @derekeder's work into topic branch - WIP
    Refactoring towards #6

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -19,40 +19,32 @@ class Meeting
     MeetingMinutes.where(meeting_id: id).first
   end
 
-  def self.load_from_apis_for_jurisdiction(municipality=nil) 
-    session = scraped_local_gov
-    if municipality
-      meetings = session[:council_agendas].find({municipality: municipality})
+  def self.load_from_apis_for_jurisdiction(municipality = nil)
+    clear_existing_meetings(municipality)
 
-      #clear out existing agendas
-      Meeting.delete_all({municipality: municipality})
-
-    else # get em all
-      meetings = session[:council_agendas].find()
-
-      #clear out existing agendas
-      Meeting.delete_all
-
-    end
-
+    meetings = if municipality
+                 scraped_local_gov[:council_agendas].find(municipality: municipality)
+               else
+                 scraped_local_gov[:council_agendas].find
+               end
     # Example agenda object returned from the scraper
     # {
     #   u'Meeting Date': {
-    #     'url': u'javascript:void(0);', 
+    #     'url': u'javascript:void(0);',
     #     'label': datetime.datetime(2013, 6, 20, 0, 0)
-    #   }, 
-    #   u'Meeting Time': u'9:00 AM', 
-    #   u'Name': u'Committee of the Whole', 
-    #   u'Meeting Location': u'Room 400', 
-    #   'fulltext': 'Public Hearing NoticeCity of PhiladelphiaThe Committee of the Whole of the Council of the City of Philadelphia will hold a Public Hearing on Thursday, June 20, 2013, at 9:00 AM, in Room 400, City Hall, to hear testimony on the following items:Resolution appointing Mjenzi Traylor to the Board of Directors of the Germantown Special Services District of Philadelphia.130504Resolution appointing Matt Canno to the Board of Directors of the Germantown Special Services District of Philadelphia.130505Resolution appointing Joseph Martin to the Board of Directors of the Germantown Special Services District of Philadelphia.130506Resolution appointing Joseph Waldo to the Board of Directors of the Germantown Special Services District of Philadelphia.130507Resolution appointing Joseph Corrigan to the Board of Directors of the Germantown Special Services District of Philadelphia.130508Resolution appointing Greg Peil to the Board of Directors of the Germantown Special Services District of Philadelphia.130509Resolution appointing Barbara Hogue to the Board of Directors of the Germantown Special Services District of Philadelphia.130510Copies of the foregoing items are available in the Office of the Chief Clerk of the Council, Room 402, City Hall.Immediately following the public hearing, a meeting of the Committee of the Whole, open to the public, will be held to consider the action to be taken on the above listed items.Michael DeckerChief ClerkCity of Philadelphia- 1 -\x0c', 
+    #   },
+    #   u'Meeting Time': u'9:00 AM',
+    #   u'Name': u'Committee of the Whole',
+    #   u'Meeting Location': u'Room 400',
+    #   'fulltext': 'Public Hearing NoticeCity of PhiladelphiaThe Committee of the Whole of the Council of the City of Philadelphia will hold a Public Hearing on Thursday, June 20, 2013, at 9:00 AM, in Room 400, City Hall, to hear testimony on the following items:Resolution appointing Mjenzi Traylor to the Board of Directors of the Germantown Special Services District of Philadelphia.130504Resolution appointing Matt Canno to the Board of Directors of the Germantown Special Services District of Philadelphia.130505Resolution appointing Joseph Martin to the Board of Directors of the Germantown Special Services District of Philadelphia.130506Resolution appointing Joseph Waldo to the Board of Directors of the Germantown Special Services District of Philadelphia.130507Resolution appointing Joseph Corrigan to the Board of Directors of the Germantown Special Services District of Philadelphia.130508Resolution appointing Greg Peil to the Board of Directors of the Germantown Special Services District of Philadelphia.130509Resolution appointing Barbara Hogue to the Board of Directors of the Germantown Special Services District of Philadelphia.130510Copies of the foregoing items are available in the Office of the Chief Clerk of the Council, Room 402, City Hall.Immediately following the public hearing, a meeting of the Committee of the Whole, open to the public, will be held to consider the action to be taken on the above listed items.Michael DeckerChief ClerkCity of Philadelphia- 1 -\x0c',
     #   u'Agenda': {
-    #     'url': u'View.ashx?M=A&ID=248602&GUID=2D23A8B0-4FB3-4EF2-A957-C6FD8FF9BDAA', 
+    #     'url': u'View.ashx?M=A&ID=248602&GUID=2D23A8B0-4FB3-4EF2-A957-C6FD8FF9BDAA',
     #     'label': u'Agenda'
-    #   }, 
+    #   },
     #   u'Minutes': {
-    #     'url': u'View.ashx?M=A&ID=248602&GUID=2D23A8B0-4FB3-4EF2-A957-C6FD8FF9BDAA', 
+    #     'url': u'View.ashx?M=A&ID=248602&GUID=2D23A8B0-4FB3-4EF2-A957-C6FD8FF9BDAA',
     #     'label': u'Minutes'
-    #   }, 
+    #   },
     # }
 
     meetings.each do |meeting_data|
@@ -68,7 +60,7 @@ class Meeting
         location: meeting_data['Meeting Location'],
         municipality: meeting_data['Municipality']
       )
-      
+
       if (meeting_data['Agenda']['url'])
         agenda = MeetingAgenda.create(
           meeting_id: meeting.id,
@@ -94,13 +86,18 @@ class Meeting
 
   private
   def self.scraped_local_gov
-    session = Moped::Session.new([ "127.0.0.1:27017" ])
-    session.use "scraped_local_gov"
-    session
+    @scraped_local_gov ||= Moped::Session.new(['127.0.0.1:27017']).use('scraped_local_gov')
+  end
+
+  def self.clear_existing_meetings(municipality = nil)
+    if municipality
+      Meeting.delete_all(municipality: municipality)
+    else
+      Meeting.delete_all
+    end
   end
 
   # class methods have do not honor private declaration
-  private_class_methods = [:scraped_local_gov]
+  private_class_methods = [:scraped_local_gov, :clear_existing_meetings]
   private_class_method *private_class_methods
-
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -4,11 +4,12 @@ describe Meeting do
   %w(meeting_date name municipality).each do |attribute|
     it {should validate_presence_of attribute}
   end
-end
 
-describe '.load_from_api_for_jurisdiction' do
-  it 'loads meetings into database given a municipality', :vcr do
-    Meeting.load_from_apis_for_jurisdiction()
-    expect(Meeting.distinct(:name).length).to be >= 27 # distinct meeting names for Philly
+  describe '.load_from_api_for_jurisdiction' do
+    it 'loads meetings into database given a municipality' do
+      Meeting.load_from_apis_for_jurisdiction()
+      # distinct meeting names for Philly
+      expect(Meeting.distinct(:name).length).to be >= 27
+    end
   end
 end
