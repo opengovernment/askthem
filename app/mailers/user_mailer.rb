0481f3e3e9d336245a24629e65952a0926929e63
Derek Eder <derek.eder@gmail.com>
2013-08-05 18:04:30 -0500

    rename user_mailer files to question_mailer, fleshed out emails with question content and links

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
-  def question_email(user, question)
-    @user = user
-    mail(:to => user.email,
-        :subject => "AskThem Question Posted")
-  end
-end
