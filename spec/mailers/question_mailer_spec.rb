96e425f6123c483b72d581c3e88f2577117df29a
Derek Eder <derek.eder@gmail.com>
2013-08-06 15:23:42 -0500

    added spec for question_mailer, nil check for person chamber attribute

--- spec/mailers/question_mailer_spec.rb
+++ spec/mailers/question_mailer_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helper"
 
-describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe QuestionMailer do
+
+  it "sends question posted e-mail" do
+    user = FactoryGirl.create(:user)
+    question = FactoryGirl.create(:question)
+    QuestionMailer.question_posted(user, question).deliver
+    last_email = ActionMailer::Base.deliveries.last
+
+    last_email.to.should == [user.email]
+    last_email.from.should eq(["develop@opengovernment.org"])
+    last_email.subject.should eq("Your Question on AskThem Has Been Posted")
+    last_email.body.encoded.should match( question_url(question.state, question.id) )
+  end
 end
