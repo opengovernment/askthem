7a654ae5e3186ba65b065eee9531c4ff20b1e9d8
acacheung <acacheung@gmail.com>
2013-08-19 12:02:55 -0400

    modal close on clicking x

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,3 +2,8 @@ $('#overlay').click(function() {
   $(this).fadeOut('fast');
   $('#modal').fadeOut('fast');
 });
+
+$('.modal-close').click(function() {
+  $('#overlay').fadeOut('fast');
+  $('#modal').fadeOut('fast');
+});
