431c6f26a65dddaf9dacdf186c9d9a25a649c8ba
Walter McGinnis <wm@waltermcginnis.com>
2013-08-30 12:51:55 +1200

    Add office details in csv safe format

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,4 +5,13 @@ module PagesHelper
     end
     links.any? ? links.join(' ; ') : nil
   end
+
+  def csv_safe_office_details_for(person)
+    offices = person.read_attribute(:offices)
+    if offices.any?
+      offices.to_s.gsub('"', "").gsub("=>", ":").gsub("[", "").gsub("\\n", " ")
+        .gsub("]", "").gsub("},", " - ").gsub("{", "").gsub(",", "")
+        .gsub("nil", "").gsub("}", "")
+    end
+  end
 end
