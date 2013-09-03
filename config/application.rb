c76001701d245597cda63c6a39e12de9cca3b0f7
Walter McGinnis <wm@waltermcginnis.com>
2013-09-03 14:11:32 +1200

    Drop observers from autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module OpenGovernment
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/app/observers)
+    # config.autoload_paths += %W(#{config.root}/app/observers)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
