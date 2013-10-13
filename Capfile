92763fcf4f4ffbdb354fee8f1aacc6d34695c0ea
Rob Chekaluk <rob@hudson.(none)>
2013-10-13 14:41:30 -0400

    First cut support for Capistrano

---
+++ Capfile
@@ -0,0 +1,4 @@
+load 'deploy'
+load 'deploy/assets'
+load 'config/deploy' # remove this line to skip loading any of the default tasks
+
---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "AskThem"
+
+default_run_options[:pty] = true
+set :scm,         :git
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
