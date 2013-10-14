db39888f98bc860e99df234bb9ccc152e9764227
Walter McGinnis <wm@waltermcginnis.com>
2013-10-15 09:29:33 +1300

    Add new default signature threshold levels

--- app/models/default_signature_threshold.rb
+++ app/models/default_signature_threshold.rb
@@ -4,7 +4,8 @@ class DefaultSignatureThreshold
   DEFAULT_VALUES = { unspecified_person: 500,
     federal: 5000,
     major_city_council: 25,
-    state: 100 }
+    state: 100,
+    governor: 200 }
 
   def initialize(person)
     @person = person if person
@@ -14,17 +15,16 @@ class DefaultSignatureThreshold
     return DEFAULT_VALUES[:unspecified_person] unless @person
 
     # @todo replace with more sophisticated calculation
-    case @person.class
-    when Person, StateLegislator
-      # HACK for council members
-      # @todo replace with CouncilMember class when implemented
-      if @person.state.include?('-')
-        DEFAULT_VALUES[:major_city_council]
-      else
-        DEFAULT_VALUES[:state]
-      end
-    when FederalLegislator
+    case @person.class.name
+    when "Councilmember"
+      DEFAULT_VALUES[:major_city_council]
+    when "FederalLegislator"
       DEFAULT_VALUES[:federal]
+    when "Governor"
+      DEFAULT_VALUES[:governor]
+    else
+      # this covers StateLegislator as default
+      DEFAULT_VALUES[:state]
     end
   end
 end
