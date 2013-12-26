78ee730ac65f0e112e0c68b96b5ba21ce27dec65
Walter McGinnis <wm@waltermcginnis.com>
2013-12-27 10:04:01 +1300

    Fix raw html output, add consistent avatar style
    Supports #187

--- app/helpers/signatures_helper.rb
+++ app/helpers/signatures_helper.rb
@@ -3,7 +3,11 @@ module SignaturesHelper
     description = "#{signer.given_name} #{signer.family_name}"
 
     if signer.image?
-      avatar_tag = image_tag(signer.image.url, size: '40x40', alt: '')
+      avatar_tag = image_tag(signer.image.url,
+                             size: "40x40",
+                             alt: "",
+                             class: "avatar-image")
+
       description = "#{avatar_tag} #{description}"
     end
 
@@ -11,6 +15,6 @@ module SignaturesHelper
       description = "#{description} (#{signer.locality}, #{signer.region.upcase})"
     end
 
-    description
+    description.html_safe
   end
 end
