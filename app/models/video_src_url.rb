1eb2ca618e9a1ded33e7ef33ce296f50f380fe12
Walter McGinnis <wm@waltermcginnis.com>
2014-01-03 14:59:27 +1300

    Add video src url value class for submitted urls
    Supports #183

---
+++ app/models/video_src_url.rb
@@ -0,0 +1,58 @@
+class VideoSrcUrl
+  VIDEO_TYPES = %w(avi mov mpg mpeg divx mp4 mpv ogv webm)
+
+  VIDEO_SITE_URL_PATTERNS = {
+    youtube: [/youtu\.be\/([^?]+)/, /youtube\.com\/watch\?v=([^&]+)/],
+    vimeo: [/vimeo.com\/([^?]+)/] }
+
+  VIDEO_SITE_VIEWER_BASES = { youtube: "//youtube.com/embed/",
+    youtube_full: "//www.youtube.com/embed/",
+    vimeo: "//player.vimeo.com/video/" }
+
+  attr_accessor :original_url, :value
+
+  def initialize(url)
+    @original_url = url
+  end
+
+  def is_video?
+    is_video_service_src? || VIDEO_TYPES.include?(extension_for(value))
+  end
+
+  def value
+    @value ||= if has_extension? || is_video_service_src?(original_url)
+                 original_url
+               else
+                 replace_with_viewer_url
+               end
+  end
+
+  private
+  def replace_with_viewer_url
+    site_key, video_id = find_site_key_and_video_id
+    "#{VIDEO_SITE_VIEWER_BASES[site_key]}#{video_id}"
+  end
+
+  def find_site_key_and_video_id
+    VIDEO_SITE_URL_PATTERNS.each do |key, value|
+      value.each do |pattern|
+        if original_url =~ pattern
+          return [key, $1]
+        end
+      end
+    end
+  end
+
+  def is_video_service_src?(url = value)
+    matches = VIDEO_SITE_VIEWER_BASES.values.select { |v| url.include?(v) }
+    matches.any?
+  end
+
+  def extension_for(file_or_url)
+    File.extname(file_or_url).sub(".", "") if file_or_url
+  end
+
+  def has_extension?
+    extension_for(original_url).present?
+  end
+end
---
+++ spec/models/video_src_url_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+describe VideoSrcUrl do
+  describe "#is_video?" do
+    it "is true if file or url is does have video extension" do
+      expect(VideoSrcUrl.new("/file.mov").is_video?).to be_true
+    end
+
+    context "when no video hosting site matches" do
+      it "is false if file or url is does't have video extension" do
+        expect(VideoSrcUrl.new("/file.png").is_video?).to be_false
+      end
+    end
+
+    it "is true when url is already direct video link from service" do
+      youtube_video = "http://www.youtube.com/embed/6IUu2KJI_5k?rel=0"
+      expect(VideoSrcUrl.new(youtube_video).is_video?).to be_true
+    end
+  end
+
+  describe "#value" do
+    it "returns original if it has an extension" do
+      original_url = "/file.mov"
+      expect(VideoSrcUrl.new(original_url).value).to eq original_url
+    end
+
+    it "returns original if it is already hosted service viewer url" do
+      original_url = "//player.vimeo.com/video/80343648?badge=0"
+      expect(VideoSrcUrl.new(original_url).value).to eq original_url
+    end
+
+    it "returns viewer url for hosted service" do
+      original_url = "http://www.youtube.com/watch?v=6IUu2KJI_5k"
+      viewer_url = "//youtube.com/embed/6IUu2KJI_5k"
+      expect(VideoSrcUrl.new(original_url).value).to eq viewer_url
+    end
+  end
+end
