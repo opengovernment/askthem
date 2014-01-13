257563d01f1e9895e165a6888008af057f8d8e55
Walter McGinnis <wm@waltermcginnis.com>
2014-01-13 16:23:56 +1300

    Refactor staff_member lookup to method for reuse

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -1,10 +1,26 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe UserRole do
   context "when global role" do
     it "has unique name" do
-      UserRole.create!(name: 'staff_member')
-      expect(UserRole.new(name: 'staff_member').valid?).to be_false
+      UserRole.create!(name: "staff_member")
+      expect(UserRole.new(name: "staff_member").valid?).to be_false
+    end
+  end
+
+  describe ".staff_members" do
+    context "when no staff_member role exists" do
+      it "returns an empty collection" do
+        expect(UserRole.staff_members).to eq Array.new
+      end
+    end
+
+    it "returns users that are staff members" do
+      UserRole.create!(name: "staff_member")
+      user = FactoryGirl.create(:user)
+      user.add_role :staff_member
+
+      expect(UserRole.staff_members).to eq [user]
     end
   end
 end
