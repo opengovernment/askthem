9776ea6b6cdf9746007e3e9bc71e01e4e67d4a07
Walter McGinnis <wm@waltermcginnis.com>
2014-01-14 15:22:48 +1300

    Refactor to more easily skip steps for non-elected
    Supports #255

--- app/models/person_detail_retriever.rb
+++ app/models/person_detail_retriever.rb
@@ -8,11 +8,32 @@ class PersonDetailRetriever
 
   # add details for our person from secondary apis
   def retrieve!
-    person_detail = InfluenceExplorerPersonDetail.new(person).person_detail
+    self.person_detail = person.person_detail
+    update_from_usa_elected_required_apis if is_usa_elected_official?
+    person_detail.save!
+  end
+
+  private
+  # assumes if state specifies jurisdiction that is within us
+  # is an elected official
+  def is_usa_elected_official?
+    return false unless person.state.present?
+
+    return true if Metadatum::Us::ABBREVIATION == person.state
+    return true if OpenGovernment::STATES.values.include?(person.state)
+
+    state_prefix = person.state.split("-").first
+    return true if OpenGovernment::STATES.values.include?(state_prefix)
+
+    false
+  end
+
+  def update_from_usa_elected_required_apis
+    self.person_detail = InfluenceExplorerPersonDetail.new(person).person_detail
+
     pvs = ProjectVoteSmartPersonDetail.new(person,
                                            person_detail: person_detail,
                                            officials: officials)
-    person_detail = pvs.person_detail
-    person_detail.save!
+    self.person_detail = pvs.person_detail
   end
 end
