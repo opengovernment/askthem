a112582dabf1f6d872cddb49c63177e0df255079
Walter McGinnis <wm@waltermcginnis.com>
2014-01-25 10:50:57 -0500

    Disable broken media thumbnailing
    Supports #245

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -30,18 +30,19 @@ class MediaUploader < CarrierWave::Uploader::Base
    ImageSrcUrl::IMAGE_TYPES + VideoSrcUrl::VIDEO_TYPES
   end
 
+  # @see https://github.com/opengovernment/askthem/issues/245
   # @todo make sure this works for both images and videos
-  version :thumb do
-    process(thumbnail: [{ format: 'png',
-                          quality: 10,
-                          size: 192,
-                          strip: true,
-                          logger: Rails.logger }])
+  # version :thumb do
+  #   process(thumbnail: [{ format: 'png',
+  #                         quality: 10,
+  #                         size: 192,
+  #                         strip: true,
+  #                         logger: Rails.logger }])
 
-    def full_filename for_file
-      png_name for_file, version_name
-    end
-  end
+  #   def full_filename for_file
+  #     png_name for_file, version_name
+  #   end
+  # end
 
   def png_name for_file, version_name
     %Q{#{version_name}_#{for_file.chomp(File.extname(for_file))}.png}
