b2bc0ee96f8d07627b39006c71397ce599e06b8d
Walter McGinnis <wm@waltermcginnis.com>
2014-02-04 08:11:42 -0500

    Update specs/cassettes  w/ Sunlight HTTPS
    Closes #309

--- spec/services/sunlight_congress_legislator_service_spec.rb
+++ spec/services/sunlight_congress_legislator_service_spec.rb
@@ -18,7 +18,7 @@ describe SunlightCongressLegislatorService do
     end
 
     def legislator
-      {"bioguide_id"=>"W000800", "birthday"=>"1947-05-02", "chamber"=>"house", "contact_form"=>"http://www.house.gov/formwelch/issue_subscribe.htm", "crp_id"=>"N00000515", "district"=>0, "facebook_id"=>"72680720883", "fax"=>"202-225-6790", "fec_ids"=>["H6VT00160", "H8VT00034"], "first_name"=>"Peter", "gender"=>"M", "govtrack_id"=>"412239", "icpsr_id"=>20750, "in_office"=>true, "last_name"=>"Welch", "middle_name"=>nil, "name_suffix"=>nil, "nickname"=>nil, "office"=>"2303 Rayburn House Office Building", "party"=>"D", "phone"=>"202-225-4115", "state"=>"VT", "state_name"=>"Vermont", "term_end"=>"2015-01-03", "term_start"=>"2013-01-03", "thomas_id"=>"01879", "title"=>"Rep", "twitter_id"=>"PeterWelch", "votesmart_id"=>51272, "website"=>"http://www.welch.house.gov", "youtube_id"=>"reppeterwelch"}
+      {"bioguide_id"=>"W000800", "birthday"=>"1947-05-02", "chamber"=>"house", "contact_form"=>"http://www.house.gov/formwelch/issue_subscribe.htm", "crp_id"=>"N00000515", "district"=>0, "facebook_id"=>"72680720883", "fax"=>"202-225-6790", "fec_ids"=>["H6VT00160", "H8VT00034"], "first_name"=>"Peter", "gender"=>"M", "govtrack_id"=>"412239", "icpsr_id"=>20750, "in_office"=>true, "last_name"=>"Welch", "middle_name"=>nil, "name_suffix"=>nil, "nickname"=>nil, "ocd_id"=>"ocd-division/country:us/state:vt", "office"=>"2303 Rayburn House Office Building", "party"=>"D", "phone"=>"202-225-4115", "state"=>"VT", "state_name"=>"Vermont", "term_end"=>"2015-01-03", "term_start"=>"2013-01-03", "thomas_id"=>"01879", "title"=>"Rep", "twitter_id"=>"PeterWelch", "votesmart_id"=>51272, "website"=>"http://www.welch.house.gov", "youtube_id"=>"reppeterwelch"}
     end
   end
 end
