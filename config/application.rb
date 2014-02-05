05d4af2a726f984883d81fc8eb20f0850c713c57
Walter McGinnis <wm@waltermcginnis.com>
2014-02-05 09:51:50 -0500

    Add default timezone & locale config
    config.i18n.enforce_available_locales = true should stop warnings in
    logs.
    
    Closes opengovernment/ops#128

--- config/application.rb
+++ config/application.rb
@@ -35,11 +35,12 @@ module OpenGovernment
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
