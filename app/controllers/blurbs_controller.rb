b28f528effd11a20e25f76a54a99c288da697669
Walter McGinnis <wm@waltermcginnis.com>
2014-03-07 10:59:52 +1300

    Capture current_user as blurb.user
    Supports #384

--- app/controllers/blurbs_controller.rb
+++ app/controllers/blurbs_controller.rb
@@ -2,8 +2,13 @@ class BlurbsController < InheritedResources::Base
   before_filter :force_http
   before_filter :authenticate_user!
   before_filter :check_manage_blurbs
+  before_filter :set_blurb_user, only: :create
 
   private
+  def set_blurb_user
+    params[:blurb][:user] = current_user
+  end
+
   def check_manage_blurbs
     unless current_user.can?(:manage_blurbs)
       raise Authority::SecurityViolation.new(current_user,
