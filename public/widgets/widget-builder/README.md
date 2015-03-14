b17615504c1e4520c7d0ff1505b17c0650104905
Walter McGinnis <wm@waltermcginnis.com>
2015-03-15 05:43:55 +1300

    Add widget builder wigdet

---
+++ public/widgets/widget-builder/README.md
@@ -0,0 +1,83 @@
+# Notes on how to add widget-builder widget to askthem code base
+
+In askthem's repo, the public/widgets directory is a bit different. It is for
+static JS widgets.
+
+However, some of these widgets are developed first as standalone Ember.js apps.
+In order for them to be distributable as widgets from http://askthem.io though,
+we need to build them, take the "concoction" (a sort of template for the app),
+and its assets and put them manually into the askthem repo's codebase.
+
+In addition, we may need to adjust some hardcoded values in the  jquery loader
+(public/widgets/widget-builder/widget.js in this case) .
+
+Convoluted, but it gives a lot of flexibility that we need to be able to
+configure with different settings via simple html and be able to have multiple
+instances of the widget on a page.
+
+Anywho, here are the steps to set up the widget-builder widget for distribution
+via AskThem.io:
+
+* in your local clone of the [widget-builder-widget](https://github.com/opengovernment/widget-builder-widget), run a build like so:
+
+```
+cd widget-builder-widget
+mkdir production # or rm -rf production/* if it already exists
+ember build -e production -o production
+```
+
+ The build will give you three things you need, the
+ widget-builder-widget-concoction.txt file, the corrsponding production/assets
+ directory which has files the app relies on, and an index.html which
+ you will base your meta config/environment meta tag on.
+
+* first copy widget-builder-widget-concoction.txt file to
+public/widgets/widget-builder/app-concoction.txt under you askthem repo
+
+* next copy the build's assets file contents to under
+public/widgets/widget-builder/assets/ - you can skip widget-builder-widget-...js file, but you want
+widget-builder-widget-...css as well as both vendor files (css and js)
+
+Something like this should do the trick (your paths may need tweaking):
+```
+cp production/assets/*.css ../../apps/askthem/public/widgets/widget-builder/assets/
+cp production/assets/vendor-*.js ../../apps/askthem/public/widgets/widget-builder/assets/
+```
+
+* we need to then edit public/widgets/widget-builder/widget.js with a number of values,
+we'll start with the assets files we just copied over:
+
+Open public/widgets/widget-builder/widget.js in a text editor. Search for "var cssUrls"
+and edit the listed files to match css files now under
+pubic/widgets/widget-builder/assets/ (changing the hash string just before the ".css"
+should do the trick).
+
+Search for "var jsUrls" and do the same for the vendor JavaScript file.
+
+* one more edit to widget.js, we need to update the config/environment meta tag
+template.
+
+_Note:_ *you only have to do this step if the widget-builder widget project's
+config/environment.js has been edited since the last time you updated
+askthem/public/widgets/widget-builder. Check commit history for the file to see.*
+
+This can be tricky and is the hardest step.
+  * back in the widget-builder-widget repo, open the production/index.html from our build
+  * notice the meta tag with name "widget-builder-widget/config/environment"? copy the *content*
+  from that tag to some temporary location where you can edit it
+  * search and replace the string "%22widget-builder-widget%22" with "%22' + tagId + '%22" in
+  the content you copied in its temporary location
+  * search and replace the string "%23address-widget-container" with
+   "%23'+ tagId +'-container" in the content you copied in its temporary location
+* back in the widget.js file in your text editor, find the function call that starts with this:
+  "_createMetaTag(tagId + '/config/environment',"
+  * replace the *2nd* argument to this function call with the *new* content with
+  "tagId" in it in widget.js
+
+* if you have doing development based on a local dev server and have
+  changed hostname for resources to something other than under
+  www.askthem.io, make sure to change it back!
+
+* last, commit your changes, push them, and deploy
+
+Your changes to the widget-builder widget is now ready on http://askthem.io.
---
+++ public/widgets/widget-builder/widget.js
@@ -0,0 +1,225 @@
+(function() {
+  // Globals for tracking instances of widget
+  // i.e. there may be more than one on the page, each called with different params
+  if (!window.AskThemWidgetBuilderWidget) { window.AskThemWidgetBuilderWidget = {}; };
+  var AskThemWidgetBuilderWidget = window.AskThemWidgetBuilderWidget;
+
+  // To keep track of which embeds we have already processed
+  if (!AskThemWidgetBuilderWidget.foundEmbeds) AskThemWidgetBuilderWidget.foundEmbeds = [];
+  var foundEmbeds = AskThemWidgetBuilderWidget.foundEmbeds;
+
+  // Localize jQuery variable
+  var jQuery;
+
+  /******** Load jQuery if not present *********/
+  if (window.jQuery === undefined || window.jQuery.fn.jquery !== '2.1.3') {
+    var script_tag = document.createElement('script');
+    script_tag.setAttribute("type","text/javascript");
+    script_tag.setAttribute("src",
+                            "http://code.jquery.com/jquery-2.1.3.min.js");
+    if (script_tag.readyState) {
+      script_tag.onreadystatechange = function () { // For old versions of IE
+        if (this.readyState === 'complete' || this.readyState === 'loaded') {
+          scriptLoadHandler();
+        }
+      };
+    } else { // Other browsers
+      script_tag.onload = scriptLoadHandler;
+    }
+    // Try to find the head, otherwise default to the documentElement
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+  } else {
+    // The jQuery version on the window is the one we want to use
+    jQuery = window.jQuery;
+    main();
+  }
+
+  /******** Called once jQuery has loaded ******/
+  function scriptLoadHandler() {
+    // Restore $ and window.jQuery to their previous values and store the
+    // new jQuery in our local jQuery variable
+    jQuery = window.jQuery.noConflict(true);
+    // Call our main function
+    main();
+  }
+
+  function htmlTemplate(tagId) {
+    var html = '<div class="at-widget">';
+    html += '<div id="' + tagId + '-container"></div>';
+    html += '</div>';
+
+    return html;
+  }
+
+  function addMetaTags(tagId, options) {
+    this._createMetaTag = function(name, content) {
+      var metaTag = jQuery("<meta>", {
+        name: name,
+        content: content
+      });
+
+      metaTag.appendTo('head');
+    }
+
+    this._isNested = function(value) {
+      return value === Object(value);
+    }
+
+    this._createMetaTagsFrom = function(namePath, value) {
+      for (var property in value) {
+        if (value.hasOwnProperty(property)) {
+          var subValue = value[property];
+
+          if (_isNested(subValue)) {
+            _createMetaTagsFrom(namePath + property + '/', subValue);
+          } else {
+            _createMetaTag(namePath + property, subValue);
+          }
+        }
+      }
+    }
+
+    // needed to set up our instance of an ember app
+    _createMetaTag(tagId + '/config/environment',
+                   '%7B%22modulePrefix%22%3A%22' + tagId + '%22%2C%22environment%22%3A%22production%22%2C%22locationType%22%3A%22none%22%2C%22contentSecurityPolicy%22%3A%7B%22default-src%22%3A%22%27none%27%22%2C%22script-src%22%3A%22%27self%27%20http%3A//ember.dev%3A35729/%22%2C%22font-src%22%3A%22%27self%27%20http%3A//askthem.dev/%22%2C%22connect-src%22%3A%22%27self%27%20http%3A//askthem.dev/%20http%3A//askthem.dev/%20ws%3A//ember.dev%3A35729/%22%2C%22img-src%22%3A%22%27self%27%20http%3A//i.embed.ly/%20http%3A//askthem.dev/%20http%3A//askthem.dev/%22%2C%22style-src%22%3A%22%27self%27%22%2C%22media-src%22%3A%22%27self%27%22%7D%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22rootElement%22%3A%22%23'+ tagId +'-container%22%2C%22askThemHost%22%3A%22askthem.dev%22%2C%22name%22%3A%22' + tagId + '%22%2C%22version%22%3A%220.0.0.568f15f7%22%7D%2C%22optionsConfig%22%3A%7B%7D%2C%22ember-apijax%22%3A%7B%22host%22%3A%22http%3A//askthem.dev%22%7D%2C%22contentSecurityPolicyHeader%22%3A%22Content-Security-Policy-Report-Only%22%2C%22exportApplicationGlobal%22%3Afalse%7D')
+
+    // add meta tags for each of our options, namespaced to our tagId
+    // they will be read by our application and set app vars
+    var initializerPath = tagId + '/initializers/options/';
+
+    _createMetaTagsFrom(initializerPath, options);
+  }
+
+
+  function appFromConcoction(tagId) {
+    // TODO: make sure that tagId is valid for our needs
+    var appConcoction;
+
+    // in dev, loading fails in chrome with security issue, due to CORs no in pow
+    // No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
+    jQuery.get('http://www.askthem.io/widgets/widget-builder/app-concoction.txt', function(data) {
+      appConcoction = data;
+
+      // var appCode = appConcoction.replace(/\\/g, '\\');
+      // appCode.replace(/PREFIX_PATTERN/g, tagId);
+
+      var appCode = appConcoction.replace(/PREFIX_PATTERN/g, tagId);
+
+      (function poll() {
+        setTimeout( function() {
+          if (AskThemWidgetBuilderWidget.jsNecessaryToComplete === 0) {
+            eval(appCode);
+          } else {
+            poll();
+          }
+        }, 100);
+      })();
+
+    }, 'text')
+  }
+
+  function addJSIfNecessary(jsUrls) {
+    jQuery.each(jsUrls, function(key, value) {
+      AskThemWidgetBuilderWidget.jsNecessaryToComplete++;
+
+      jQuery.ajax({
+        url: value,
+        dataType: "script",
+        cache: true
+      }).done(function() {
+        AskThemWidgetBuilderWidget.jsNecessaryToComplete--;
+      });
+    });
+  }
+
+  function addCssLinksIfNecessary(cssUrls) {
+    jQuery.each(cssUrls, function(key, value) {
+      var cssSelector = "link[href='"+ value + "']";
+
+      if (!jQuery(cssSelector).length) {
+        var cssLink = jQuery("<link>", {
+          rel: "stylesheet",
+          type: "text/css",
+          href: value
+        });
+
+        cssLink.appendTo('head');
+      }
+    });
+  }
+
+  /******** Our main function ********/
+  function main() {
+    jQuery(document).ready(function($) {
+      // We can use jQuery here
+
+      // load css, only if it hasn't been loaded
+      var cssUrls = ['http://www.askthem.io/widgets/widget-builder/assets/vendor-ba26407e909fb4a64f1f0a2ade32443c.css',
+                     'http://www.askthem.io/widgets/widget-builder/assets/widget-builder-widget-fc4b8ca8e839dd39d2d9b2f4af0fb4c6.css'];
+
+      addCssLinksIfNecessary(cssUrls);
+
+      var jsUrls = ['http://www.askthem.io/widgets/widget-builder/assets/vendor-682986e0ca4d48c96eb18b0408334ac6.js'];
+
+      // track if all our scripts have loaded globally
+      // we increment and decrement within addJSIfNecessary
+      if (!AskThemWidgetBuilderWidget.jsNecessaryToComplete) AskThemWidgetBuilderWidget.jsNecessaryToComplete = 0;
+
+      addJSIfNecessary(jsUrls);
+
+      jQuery('.at-widget-loader').each(function(i) {
+        if (foundEmbeds.indexOf(this) < 0) {
+          // parse our parameters
+          // set defaults, use data attributes where possible
+          // hidden divs where we need html
+          var tagId = 'at-widget-builder-' + i.toString();
+          if (jQuery(this).data('tag-id') && jQuery(this).data('tag-id').length > 0) tagId = jQuery(this).data('tag-id');
+
+          var tagIdSelector = "#"+tagId;
+
+          var paramsDiv;
+          jQuery('div.at-widget-attributes').each(function () {
+            var associatedTagId = jQuery(this).data('tag-id');
+            if (typeof associatedTagId !== "undefined" && associatedTagId === tagId) {
+              paramsDiv = this;
+            }
+          })
+
+          var headline = '';
+
+          if(paramsDiv) {
+            if (jQuery(paramsDiv).find('.intro-headline').length > 0) headline = jQuery(paramsDiv).find('.intro-headline').html();
+          } else if (typeof jQuery(this).data('intro-headline') !== "undefined") {
+            headline = jQuery(this).data('intro-headline');
+          }
+
+          var explanatory = '';
+
+          if(paramsDiv) {
+            if (jQuery(paramsDiv).find('.intro-explanatory').length > 0) questionSummary = jQuery(paramsDiv).find('.intro-explanatory').html();
+          } else if (typeof jQuery(this).data('intro-explanatory') !== "undefined") {
+            explanatory = jQuery(this).data('intro-explanatory');
+          }
+
+          var options = {
+            intro: {
+              headline: headline,
+              explanatory: explanatory
+            }
+          };
+
+          // add our HTML (including meta tags)
+          addMetaTags(tagId, options);
+
+          jQuery(this).after(htmlTemplate(tagId));
+
+          // set up our instance of Ember with its custom namespace
+          // based on a concoction (sort of like a template for entire app)
+          appFromConcoction(tagId);
+
+          foundEmbeds.push(this);
+        }
+      })
+    });
+  }
+})(); // We call our anonymous function immediately
